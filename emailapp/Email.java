2df9cfd23f1394caaaa6db05ed5e5534d8a94f9c
Sushant <khanalsushan14@gmail.com>
2026-05-16 21:51:19 +0545

    Added constructor to receive first name and last name, asking for department and random password generation

--- emailapp/Email.java
+++ emailapp/Email.java
@@ -1,5 +1,8 @@
 package emailapp;
 
+import java.util.Scanner;
+
+
 public class Email{
     private String firstName;
     private String lastName;
@@ -9,10 +12,39 @@ public class Email{
     private String alternateEmail;
 
     // Constructor to receive the first name and last name
+    public Email(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        System.out.println("Email Created: " + this.firstName + " " + this.lastName);
+
+        // // Call a method asking for the department - return the department
+        this.department = setDepartment();
+
+        // // Call a method that returns a random password
+        this.password = randomPassword(8);
+    }
 
     //Ask for the department
+    private String setDepartment() {
+        System.out.print("Department Codes:\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter department code: ");
+        Scanner in = new Scanner(System.in);
+        int depChoice = in.nextInt();
+        if (depChoice == 1) { return "sales"; }
+        else if (depChoice == 2) { return "dev"; }
+        else if (depChoice == 3) { return "acct"; }
+        else { return ""; }
+    }
 
     //Generate a random password
+    private String randomPassword(int length) {
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%";
+        char[] password = new char[length];
+        for (int i=0; i<length; i++) {
+            int rand = (int) (Math.random() * passwordSet.length());
+            password[i] = passwordSet.charAt(rand);
+        }
+        return new String(password);
+    }
 
     //Combine elements to generate email
 
